e649699f0279a12cda8db7a3db4768be2f6dd157
Hudson C. Dalpra <dalpra.hcd@gmail.com>
2025-12-02 21:39:25 +1300

    solved: day 02: part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -15,7 +15,17 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let line = input.lines().next()?;
+
+    let ranges = parse_ranges(line);
+
+    let invalid_ids_sum: u64 = ranges
+        .iter()
+        .flat_map(|(start, end)| *start..=*end)
+        .filter(|id| is_repeated_at_least_twice(&id.to_string()))
+        .sum();
+
+    Some(invalid_ids_sum)
 }
 
 fn parse_ranges(line: &str) -> Vec<(u64, u64)> {
@@ -38,6 +48,25 @@ fn is_repeated_twice(id: &str) -> bool {
     first == second
 }
 
+fn is_repeated_at_least_twice(id: &str) -> bool {
+    let len = id.len();
+    let bytes = id.as_bytes();
+
+    for pattern_len in 1..=len / 2 {
+        if !len.is_multiple_of(pattern_len) {
+            continue;
+        }
+
+        let pattern = &bytes[..pattern_len];
+
+        if bytes.chunks(pattern_len).all(|chunk| chunk == pattern) {
+            return true;
+        }
+    }
+
+    false
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -51,6 +80,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4174379265));
     }
 }
