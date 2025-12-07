ca5fc3e767b2032a7bd9cf315806ce264114be8a
Hudson C. Dalpra <dalpra.hcd@gmail.com>
2025-12-07 19:21:52 +1300

    solved: day 06: part 1: but very ugly

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,10 +1,49 @@
 advent_of_code::solution!(6);
 
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    let mut homework: Vec<&str> = Vec::new();
+    for line in input.lines() {
+        if !line.is_empty() {
+            homework.push(line);
+        }
+    }
+
+    let operations: Vec<&str> = homework.last()?.split_ascii_whitespace().collect();
+    let mut numbers: Vec<Vec<u64>> = Vec::new();
+    for line in homework.iter().take(homework.len() - 1) {
+        let ns: Vec<u64> = line
+            .split_ascii_whitespace()
+            .filter_map(|s| s.parse::<u64>().ok())
+            .collect();
+        numbers.push(ns);
+    }
+
+    let mut grand_total = 0u64;
+    for (idx, op) in operations.iter().enumerate() {
+        let problem: u64 = match *op {
+            "+" => {
+                let mut sum = 0u64;
+                for row in &numbers {
+                    sum += row[idx];
+                }
+                sum
+            }
+            "*" => {
+                let mut prod = 1u64;
+                for row in &numbers {
+                    prod *= row[idx];
+                }
+                prod
+            }
+            _ => unreachable!(),
+        };
+        grand_total += problem;
+    }
+
+    Some(grand_total)
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
+pub fn part_two(_input: &str) -> Option<u64> {
     None
 }
 
