f328541d53c1d20bb117ac3c1fbb70b2a9d58572
Hudson C. Dalpra <dalpra.hcd@gmail.com>
2025-12-07 13:42:06 +1300

    solved: day 05: part 2: but too slow

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 advent_of_code::solution!(5);
 
 pub fn part_one(input: &str) -> Option<u64> {
@@ -35,7 +37,26 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut ranges: Vec<(u64, u64)> = Vec::new();
+
+    for line in input.lines() {
+        if line.is_empty() {
+            break;
+        }
+
+        let (start, end) = line.split_once("-")?;
+        ranges.push((start.parse::<u64>().ok()?, end.parse::<u64>().ok()?));
+    }
+
+    let mut fresh: HashSet<u64> = HashSet::new();
+
+    for (start, end) in ranges {
+        for ingredient in start..=end {
+            fresh.insert(ingredient);
+        }
+    }
+
+    Some(fresh.len() as u64)
 }
 
 #[cfg(test)]
